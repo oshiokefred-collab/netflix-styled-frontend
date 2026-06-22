5e35d8a1171479290ec00dd860c8534cf8eb3604
oshiokefred-collab <oshiokefred@gmail.com>
2026-06-22 21:08:20 +0100

    Point frontend to EKS backend LoadBalancer

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://3.88.9.221:30080',
+    baseURL: 'http://ad24fc7cf37624cfab4e4b1b34b69c02-1752615503.us-east-1.elb.amazonaws.com:8080',
     headers: {
         'Content-Type': 'application/json',
     },
